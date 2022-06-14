108bfeecd1727a66fca57b1ec1e0f7286d0cee66
Facupelli <facundopellicer4@gmail.com>
2022-06-14 20:57:02 -0300

    freelancecard component

--- client/src/components/Freelance/Freelance.jsx
+++ client/src/components/Freelance/Freelance.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import Project from "../Projects/Project/Project";
 import alalaCampo from "../../images/alalaCasa1.png";
+import alalaHome from "../../images/alalaHome.png";
 import donutshomero from "../../images/donutshomero.png";
-import s from "./Freelance.module.scss";
+import donutshomerocart from "../../images/donutshomerCart.png";
 import Section from "../Section/Section";
+import FreelanceCard from "./FreelanceCard/FreelanceCard";
+import s from "./Freelance.module.scss";
 
 export default function Freelance() {
   return (
     <Section title="Freelance">
       <div className={s.works}>
-        <Project
+        {/* <Project
           title="ALALA CAMPO"
           subtitle="info website"
           techs={["Next.js", "React", "Sass", "MongoDB", "Mongoose"]}
@@ -24,9 +27,25 @@ export default function Freelance() {
             access as admin and book dates on the calendar for each house.
             Responsive design.
           </p>
-        </Project>
+        </Project> */}
 
-        <Project
+        <FreelanceCard
+          img1={alalaHome}
+          img2={alalaCampo}
+          techs={["Next.js", "React", "Sass", "MongoDB", "Mongoose"]}
+          github="https://github.com/Facupelli/AlalaCampo"
+          deploy="https://www.alalacampo.com/"
+        >
+          <p className={s.children}>
+            Designed this site for Alala Campo. It is a place were there are
+            three country houses for rent. Now customers can view the place
+            before they go, check availability and have contact. The owner can
+            access as admin and book dates on the calendar for each house.
+            Responsive design.
+          </p>
+        </FreelanceCard>
+
+        {/* <Project
           title="DONUTS HOMERO"
           subtitle="info website"
           techs={[
@@ -48,7 +67,30 @@ export default function Freelance() {
             are updated in real-time with each purchase made on the site.
             Responsive design.
           </p>
-        </Project>
+        </Project> */}
+
+        <FreelanceCard
+          img1={donutshomero}
+          img2={donutshomerocart}
+          techs={[
+            "Next.js",
+            "Sass",
+            "Redux-toolkit",
+            "Prisma",
+            "Supabase",
+            "PostgreSQL",
+          ]}
+          github="https://github.com/Facupelli/donutshomero"
+          deploy="https://donutshomero.vercel.app/"
+        >
+          <p className={s.children}>
+            This app is a donut e-commerce. It handles stock and users.
+            Customers can now buy donuts online. The company owner has admin
+            access where he can view and control the stock and the orders which
+            are updated in real-time with each purchase made on the site.
+            Responsive design.
+          </p>
+        </FreelanceCard>
       </div>
     </Section>
   );
--- client/src/components/Section/Section.jsx
+++ client/src/components/Section/Section.jsx
@@ -5,7 +5,7 @@ export default function Section({ title, children }) {
   return (
     <section className={s.container}>
       <div className={s.title}>
-        <p>{title}</p>
+        <p>{title.toUpperCase()}</p>
       </div>
       {children}
     </section>
