35e25c3a16c5356b3063595b9056a0e478aae8c2
Facupelli <facundopellicer4@gmail.com>
2022-06-16 10:56:17 -0300

    lerp to contact info

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -19,12 +19,19 @@ export default function Contact({ contactRef }) {
   return (
     <Section title="Get In Touch">
       <div className={s.info}>
-        <p>
+        <p data-scroll data-scroll-delay="0.04" data-scroll-speed="2">
           I am currently looking for my first opportunity to contribute on
           challenging projects.
         </p>
-        <p>I am available to work!</p>
-        <div className={s.button_container}>
+        <p data-scroll data-scroll-delay="0.06" data-scroll-speed="2">
+          I am available to work!
+        </p>
+        <div
+          className={s.button_container}
+          data-scroll
+          data-scroll-delay="0.1"
+          data-scroll-speed="2"
+        >
           <Mailto email="facundopellicer4@gmail.com">Mail me!</Mailto>
         </div>
       </div>
